c26afa057e6109cc5842dc73486d2490db15b04f
Herosb <32846885+Herosb@users.noreply.github.com>
2017-11-05 16:08:52 +0100

    zadanie nr 4

---
+++ Zadanie4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Zadanie4 {
+	public static void main(String[] args) {
+		
+		
+			Scanner xy = new Scanner(System.in);
+		int liczba = xy.nextInt();
+		int suma = 0;
+		do
+		liczba = suma + liczba;
+		
+		
+		while(liczba!=0);
+		System.out.print(suma);
+		}
+		
+	}
+
+		 
+	
+
