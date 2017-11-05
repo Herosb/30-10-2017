0b54635f74ea110a5de1b222f09d7c7d6e421d12
Herosb <32846885+Herosb@users.noreply.github.com>
2017-11-05 16:01:50 +0100

    Add files via upload
    pierwsze skonczone

---
+++ Zadanie1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Zadanie1{
+
+	public static void main(String[] args) 
+	{
+		int a;
+		String t;
+		Scanner in = new Scanner(System.in);
+		
+		t = in.nextLine();
+		a = Integer.parseInt(t);
+	
+		for(int i=1;i<=a;i+=2)
+			System.out.print(i+" ");
+	 }
+}
